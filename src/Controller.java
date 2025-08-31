0b39205eaef1a6da7a2cd65ed3d22cad331dba27
Hola2212 <juanpaflores06@gmail.com>
2025-08-30 22:23:48 -0600

    Se corrigieron errore en controller. No se logro terminar persistencia de datos. Falta visualizador.

--- src/Controller.java
+++ src/Controller.java
@@ -1,33 +1,36 @@
+/*
+ * Se debe corregir el manejo de persistencia de datos, ya que no se edita la informacion necesaria
+ */
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Controller{
-    private Scanner teclado = new Scanner(System.in);
+    
     private ArrayList<Trainer> entrenadores = new ArrayList<>();
     private ArrayList<Client> clientes = new ArrayList<>();
     private ArrayList<Routine> rutinas = new ArrayList<>();
     private ArrayList<Exercise> ejercicios = new ArrayList<>();
     private List<String> equivalenteEjercicios = new ArrayList<>();
-    public Controller() {
+    public Controller() {               //Inicializador de la clase
         setInitialTrainers();
+        setInitialClients();
+        setInitialExercises();
+        setInitialRoutines();
     }
-    public void setInitialTrainers(){
+    public void setInitialTrainers(){           //Obtiene persistencia de datos para entrenadores
         String archCSV = "D:\\listaEntrenadores.csv";
         List<List<String>> data = new ArrayList<>();
-        // Try-with-resources to ensure the reader gets closed automatically
         try (BufferedReader br = new BufferedReader(new FileReader(archCSV))) {
             String line;
-            // Read each line from the file
             while ((line = br.readLine()) != null) {
-                // Split the line by comma and convert to a List
                 String[] values = line.split(",");
                 List<String> lineData = Arrays.asList(values);
-                // Add the line data to our main list
                 if (lineData.get(0) == "Nombre") {}
                 else{
                 data.add(lineData);
@@ -45,15 +48,11 @@ public class Controller{
     public void setInitialClients(){
         String archCSV = "D:\\listaClientes.csv";
         List<List<String>> data = new ArrayList<>();
-        // Try-with-resources to ensure the reader gets closed automatically
         try (BufferedReader br = new BufferedReader(new FileReader(archCSV))) {
             String line;
-            // Read each line from the file
             while ((line = br.readLine()) != null) {
-                // Split the line by comma and convert to a List
                 String[] values = line.split(",");
                 List<String> lineData = Arrays.asList(values);
-                // Add the line data to our main list
                 if (lineData.get(0) == "Nombre") {}
                 else{
                 data.add(lineData);
@@ -70,15 +69,11 @@ public class Controller{
     public void setInitialExercises(){
         String archCSV = "D:\\listaEjercicios.csv";
         List<List<String>> data = new ArrayList<>();
-        // Try-with-resources to ensure the reader gets closed automatically
         try (BufferedReader br = new BufferedReader(new FileReader(archCSV))) {
             String line;
-            // Read each line from the file
             while ((line = br.readLine()) != null) {
-                // Split the line by comma and convert to a List
                 String[] values = line.split(",");
                 List<String> lineData = Arrays.asList(values);
-                // Add the line data to our main list
                 if (lineData.get(0) == "Nombre") {}
                 else{
                 data.add(lineData);
@@ -97,15 +92,11 @@ public class Controller{
     public void setInitialRoutines(){
         String archCSV = "D:\\listaRutinas.csv";
         List<List<String>> data = new ArrayList<>();
-        // Try-with-resources to ensure the reader gets closed automatically
         try (BufferedReader br = new BufferedReader(new FileReader(archCSV))) {
             String line;
-            // Read each line from the file
             while ((line = br.readLine()) != null) {
-                // Split the line by comma and convert to a List
                 String[] values = line.split(",");
                 List<String> lineData = Arrays.asList(values);
-                // Add the line data to our main list
                 if (lineData.get(0) == "Nombre") {}
                 else{
                 data.add(lineData);
@@ -130,6 +121,13 @@ public class Controller{
             entrenadores.get( entrenadores.indexOf(nombre));
         } catch (Exception e) {
             entrenadores.add(new Trainer(nombre,0));
+            String filePath = "listaEntrenadores.csv";
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                writer.write(nombre+",0");
+                writer.newLine();
+            }
+            catch (Exception e2) {
+            }
         }
     }
     public void agregarRutina(String nombre, Exercise[] ejerciciosRutina, int cantClientesRutina){
@@ -137,6 +135,14 @@ public class Controller{
             rutinas.get(rutinas.indexOf(nombre));
         } catch (Exception e) {
             rutinas.add(new Routine(nombre, ejerciciosRutina, cantClientesRutina));
+            String filePath = "listaRutinas.csv";
+            Routine rutinaActual = rutinas.get(rutinas.size()-1);
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                writer.write(nombre+","+ rutinaActual.getNombreEjercicio(0)+","+rutinaActual.getNombreEjercicio(1)+","+rutinaActual.getNombreEjercicio(2)+","+rutinaActual.getNombreEjercicio(3)+","+rutinaActual.getNombreEjercicio(4)+","+rutinaActual.getNombreEjercicio(5)+","+ Integer.toString(cantClientesRutina));
+                writer.newLine();
+            }
+            catch (Exception e2) {
+            }
         }
     }
     public void agregarEjercicio(String nombre, int repeticiones, int series){
@@ -144,6 +150,13 @@ public class Controller{
             ejercicios.get(ejercicios.indexOf(nombre));
         } catch (Exception e) {
             ejercicios.add(new Exercise(nombre, repeticiones, series));
+            String filePath = "listaEjercicios.csv";
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                writer.write(nombre+","+Integer.toString(repeticiones)+","+Integer.toString(series)+",");
+                writer.newLine();
+            }
+            catch (Exception e2) {
+            }
         }
     }
     
@@ -165,6 +178,13 @@ public class Controller{
                     nuevaInfoRutina.setRoutine(1);
                     rutinas.set(i,nuevaInfoRutina);
                 }
+            }
+            String filePath = "listaClientes.csv";
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                writer.write(nombre+","+entrenador+","+rutina);
+                writer.newLine();
+            }
+            catch (Exception e2) {
             }
         }
     }
--- src/main.java
+++ src/main.java
@@ -1,7 +1,13 @@
+/*
+ * Me demore demasiado trabajando la persiostencia de datos, que no me dio tiempo de trabajar el visualizador a tiempo para la entrega. 
+ */
 import java.io.PrintStream;
+import java.util.Scanner;
 public class main{
     private PrintStream Consola = System.out;
-    public void main(String[] args) {
-        Controller controlador = new Controller();
+    private Scanner teclado = new Scanner(System.in);
+    public Controller controlador = new Controller();
+    public void main() {
+        Consola.println("Bienvenido al manejo de usuarios en el gimnasio.");
     }
 }
